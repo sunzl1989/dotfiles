fe25c36ffb504d28229ac332ec9225c81b4a5bdc
Davide D'Agostino <info@daddye.it>
2013-02-25 17:21:19 -0800

    Added lein

---
+++ .lein/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-outdated "1.0.0"]
+                  [lein-bin "0.3.0"]
+                  [lein-pprint "1.1.1"]
+                  [lein-kibit "0.0.7"]
+                  [lein-notes "0.0.2"]]}}
